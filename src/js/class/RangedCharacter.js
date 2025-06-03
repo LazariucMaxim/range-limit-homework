95bf87a69192ffc21b566d31228db284c108731d
Lazariuc Maxim <lazariucmaxim@yandex.ru>
2025-06-03 23:25:30 +0300

    Maximum coverage

--- src/js/class/RangedCharacter.js
+++ src/js/class/RangedCharacter.js
@@ -18,9 +18,7 @@ export default class RangedCharacter extends Character{
         // this.attack[x]; x-distance; 1<=x<=5
         let attacks = new Uint16Array(6);
         for (let i in attacks) {
-            if (i) {
-                attacks[i] = this._attack*(1-0.1*(i-1))-this.stoned*Math.log2(i)*5;
-            }
+            attacks[i] = (i>0)*(this._attack*(1-0.1*(i-1))-this.stoned*Math.log2(i)*5);
         }
         return attacks;
     }
